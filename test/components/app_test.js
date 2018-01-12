583d5a8c19fbefd48745a164e04af5520c4d4d98
quebez <marty.janik@gmail.com>
2018-01-12 16:30:10 +0100

    5th lesson

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,4 +1,4 @@
-import { renderComponent , expect } from '../test_helper';
+/*import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App' , () => {
@@ -12,3 +12,24 @@ describe('App' , () => {
     expect(component).to.exist;
   });
 });
+*/
+
+//Purpose: to test src/components/app
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+//group similar tests ('App' section)
+describe('App', () => {
+
+  //test a single attribute of a target ('description')
+  it('shows the correct text', () => {
+
+    //create an instance of App
+    const component = renderComponent(App);
+
+    //assertion about target
+    expect(component).to.contain('React simple starter');
+  })
+})
+
+
